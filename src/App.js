ac801649404c156161d980866c2e41942e23720b
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-07-05 20:54:29 +0100

    added Message component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import Greet from './components/greet'
-import Welcome from './components/Welcome';
+import Message from './components/Message'
+
 function App() {
   return (
     <div className="App">
-      <Greet name="Ilyas El Mabrouki">
-        <p>TEST</p>
-      </Greet>
-      <Welcome name="Ilyas El Mabrouki">
-        <p>TEST</p>
-      </Welcome>
+      <Message></Message>
     </div>
   );
 }
