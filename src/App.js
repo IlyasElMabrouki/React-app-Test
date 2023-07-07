cd25a4b197725a35b96a002ee321712277cb0c82
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-07-07 15:08:46 +0100

    add counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Message from './components/Message'
+import Counter from './Components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Message></Message>
+      <Counter></Counter>
     </div>
   );
 }
